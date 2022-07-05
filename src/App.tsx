c1dd357008e3cc954946ef2cbc9677c98a644bb6
Andressa Reis Araujo <andressareis98@outlook.com>
2022-07-05 17:33:32 -0300

    feat: edit event status using recoil

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,6 @@ function App() {
           <Card>
             <ListaDeEventos
               aoFiltroAplicado={aplicarFiltro}
-              aoAlterarStatus={alterarStatusEvento}
             />
           </Card>
         </div>
