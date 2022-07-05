03a9e9ea1f3b640aa23248b282dc2cf9ab59dab8
Andressa Reis Araujo <andressareis98@outlook.com>
2022-07-05 15:24:03 -0300

    feat: set event on recoil list

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
       <div className={style.App}>
         <div className={style.Coluna}>
           <Card>
-            <Formulario aoSalvar={adicionarEvento} />
+            <Formulario />
           </Card>
           <hr />
           <Card>
